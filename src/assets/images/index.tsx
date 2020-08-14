78b47eb61a4c1b7da9b28e65f900fbbb75e65ca2
Jordan Guinn <jordan@skylight.digital>
2020-08-14 08:48:20 -0700

    Add OEC logo icon w/green check (#43)
    * Add CSV for logo with check
    
    * We should probably export all the stuff in this library
    
    * Will this do it?
    
    * Bump version again?  idk
    
    * Now we don't have to build this thing manually all the time
    
    * Don't need this anymore, now that we have that prepare script
    
    * Remove version bump
    
    * Let's the use the actual download arrow

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SVGInline } from './SVGInline';
+
 // USWDS imports
 import arrowDown from 'uswds/dist/img/arrow-down.svg';
 import error from 'uswds/dist/img/alerts/error.svg';
 import info from 'uswds/dist/img/alerts/info.svg';
 import success from 'uswds/dist/img/alerts/success.svg';
+
 // Local imports
 import angleArrowDown from './angleArrowDown.svg'
 import arrowRight from './arrowRight.svg'
@@ -12,11 +14,14 @@ import calendar from './calendar.svg'
 import downArrowCircle from './downArrowCircle.svg'
 import hero from './hero.svg'
 import logo from './logo.svg'
+import logoWithCheck from './logoWithCheck.svg';
 import pencil from './pencil.svg'
 import plusCircle from './plusCircle.svg'
 import teacherWithChalkboard from './teacherWithChalkboard.svg'
+import downloadArrow from './download.svg';
 
 export const ArrowDown = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={arrowDown} />
+export const DownloadArrow = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={downloadArrow} />
 export const Error = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={error} />
 export const Info = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={info} />
 export const Success = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={success} />
@@ -26,6 +31,7 @@ export const Calendar = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svg
 export const DownArrowCircle = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={downArrowCircle} />
 export const Hero = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={hero} />
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={logo} />
+export const LogoWithCheck = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={logoWithCheck} />
 export const Pencil = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={pencil} />
 export const PlusCircle = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={plusCircle} />
 export const TeacherWithChalkboard = (props: React.SVGProps<SVGSVGElement>) => <SVGInline svgProps={props} url={teacherWithChalkboard} />
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * from './assets/images';
 export * from './components'
 export * from './hooks'
