5404771c0ee6752af101e2ea7d65199954e0d764
bamader <49412165+bamader@users.noreply.github.com>
2020-08-24 12:56:13 -0400

    Clean, working tab-based form page (#47)
    * Clean, working tab-based form page
    
    * History eliminates passed-in state
    
    We don't need to push tab switches into the history, since executing a "back" button-like command should theoretically take you back to the page before the TabNav rather than the previous tab you were on (maybe this is just me, but I *hate* when websites use slideshows and then hitting back after you've clicked through takes you to a previous image--that's the analogy I'm working off of). Also, pushing to history disrupts any state passed in via links (currently, the edit record page is the only thing that uses a TabNav and it relies on state to keep track of which child in which report you're actually editing).
    
    * Added hover UI element
    
    * Added focus styling to mirror hover
    
    * De-duplicated hover and focus code

--- src/components/TabNav/TabNav.tsx
+++ src/components/TabNav/TabNav.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
-import { useHistory } from 'react-router';
 
 export type TabItemProps = {
 	id: string;
@@ -16,20 +15,13 @@ export type TabNavProps = {
 export const TabNav: React.FC<TabNavProps> = ({ items, activeId }) => {
 	const startingIndex = items.findIndex((item) => item.id === activeId);
 	const [activeTabIndex, setActiveTabIndex] = useState(startingIndex < 0 ? 0 : startingIndex);
-	const history = useHistory();
 
-	useEffect(() => {
-		const activeIndex = items.findIndex((item) => item.id === activeId);
-		const activeIndexOrCurrent = activeIndex < 0 ? activeTabIndex : activeIndex;
-		setActiveTabIndex(activeIndexOrCurrent);
-	}, [activeId, activeTabIndex, items]);
-
-	useEffect(() => {
-		history.push(`#${items[activeTabIndex].id}`);
-	}, [activeTabIndex, history, items]);
-
+	// Cleanest way to handle tab switching.
+	// Don't need to use effects because we don't have to actually
+	// run functions after the DOM renders.
+	// This prevents getting caught in infinite state loops.
 	const onClick = (index: number) => {
-		setActiveTabIndex(index);
+        setActiveTabIndex(index);
 	};
 
 	const tabs = items.map(({ text }, index) => (
