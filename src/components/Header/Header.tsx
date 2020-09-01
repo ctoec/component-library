e64f8e908bf3e589c299644f73f8e853b2234ece
Melanie <melanie.mazanec@gmail.com>
2020-08-31 22:06:20 -0400

    Rewrite header to use hooks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { matchPath } from 'react-router';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { matchPath, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import { NavItem, NavItemProps } from './NavItem';
 import closeIcon from 'uswds/src/img/close.svg';
 import cx from 'classnames';
 import { DropDownNavItemProps } from '../SimpleHeader/DropDownNavItem';
+import { useState, useEffect } from '@storybook/addons';
 
 export type HeaderProps = {
 	primaryTitle: string;
@@ -33,117 +33,99 @@ const setActiveStateOfNavItem = function (item: NavItemProps, index: number, pat
 	return { ...item, active };
 };
 
-class HeaderWithoutRouter extends React.Component<HeaderProps & RouteComponentProps, HeaderState> {
-	state = { menuIsVisible: false };
+export function Header({ primaryTitle,
+	secondaryTitle,
+	navItems,
+	loginPath = '/login',
+	logoutPath = '/logout',
+	userFirstName,
+}: HeaderProps) {
+	const [mobileMenuIsVisible, setMobileMenuIsVisible] = useState(false);
+	const location = useLocation();
 
-	showMenu = () => {
-		this.setState({ menuIsVisible: true });
-	};
-
-	hideMenu = () => {
-		this.setState({ menuIsVisible: false });
-	};
-
-	componentDidMount() {
-		window.addEventListener('resize', this.hideMenu);
+	function hideMenu() {
+		setMobileMenuIsVisible(false)
 	}
-
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.hideMenu);
-	}
-
-	componentDidUpdate(prevProps: RouteComponentProps) {
-		if (this.props.location.pathname !== prevProps.location.pathname) {
-			this.hideMenu();
+	useEffect(() => {
+		window.addEventListener('resize', hideMenu);
+		return function cleanup() {
+			window.removeEventListener('resize', hideMenu);
 		}
-	}
+	}, [])
 
-	render() {
-		const {
-			primaryTitle,
-			secondaryTitle,
-			navItems,
-			loginPath = '/login',
-			logoutPath = '/logout',
-			userFirstName,
-			location,
-		} = this.props;
 
-		const primaryNavItems = navItems
-			.filter((item) => item.type === 'primary')
-			.map((item, index) => setActiveStateOfNavItem(item, index, location.pathname));
+	const primaryNavItems = navItems
+		.filter((item) => item.type === 'primary')
+		.map((item, index) => setActiveStateOfNavItem(item, index, location.pathname));
 
-		const secondaryNavItems = navItems.filter((item) => item.type === 'secondary');
+	const secondaryNavItems = navItems.filter((item) => item.type === 'secondary');
 
-		return (
+	return (
+		<div
+			className={cx({
+				'usa-js-mobile-nav--active': mobileMenuIsVisible,
+			})}
+		>
 			<div
-				className={cx({
-					'usa-js-mobile-nav--active': this.state.menuIsVisible,
-				})}
+				data-testid="overlay"
+				className={cx('usa-overlay', { 'is-visible': mobileMenuIsVisible })}
+				onClick={hideMenu}
+			></div>
+			<header
+				className={cx('usa-header', 'usa-header--extended', 'usa-header--oec-default')}
+				role="banner"
 			>
-				<div
-					data-testid="overlay"
-					className={cx('usa-overlay', { 'is-visible': this.state.menuIsVisible })}
-					onClick={this.hideMenu}
-				></div>
-				<header
-					className={cx('usa-header', 'usa-header--extended', 'usa-header--oec-default')}
-					role="banner"
-				>
-					<div className={cx('usa-navbar')}>
-						<div className={cx('usa-logo')} id="extended-logo">
-							<Link to="/" aria-label={`${primaryTitle} home`}>
-								<div className={cx('usa-logo__text', 'display-flex', 'flex-align-center')}>
-									<div>
-										<div
-											className={cx('primary-title', {
-												'primary-title--only': !secondaryTitle,
-											})}
-										>
-											{primaryTitle}
-										</div>
-										{secondaryTitle && <div className="secondary-title">{secondaryTitle}</div>}
+				<div className={cx('usa-navbar')}>
+					<div className={cx('usa-logo')} id="extended-logo">
+						<Link to="/" aria-label={`${primaryTitle} home`}>
+							<div className={cx('usa-logo__text', 'display-flex', 'flex-align-center')}>
+								<div>
+									<div
+										className={cx('primary-title', {
+											'primary-title--only': !secondaryTitle,
+										})}
+									>
+										{primaryTitle}
 									</div>
-								</div>
-							</Link>
-						</div>
-						<button className={cx('usa-menu-btn')} onClick={this.showMenu}>
-							Menu
-						</button>
-					</div>
-					<nav
-						className={cx('usa-nav', { 'is-visible': this.state.menuIsVisible })}
-					>
-						<div className={cx('usa-nav__inner')}>
-							<button className={cx('usa-nav__close')} onClick={this.hideMenu}>
-								<img src={closeIcon} alt="close" />
-							</button>
-							<ul className={cx('usa-nav__primary usa-accordion')}>
-								{primaryNavItems.map((item, index) => (
-									<NavItem {...item} key={index} />
-								))}
-							</ul>
-							<div className={cx('usa-nav__secondary', 'usa-nav__secondary--extended')}>
-								<ul className={cx('usa-nav__secondary-links')}>
-									{secondaryNavItems.map((item, index) => (
-										<NavItem {...item} key={index} />
-									))}
-									{userFirstName && <NavItem type="secondary" title="Log out" path={logoutPath} />}
-								</ul>
-								<div className={cx('oec-logged-in-user')}>
-									{userFirstName ? (
-										<span>Hi, {userFirstName}.</span>
-									) : (
-											<Link to={loginPath}>Sign in</Link>
-										)}
+									{secondaryTitle && <div className="secondary-title">{secondaryTitle}</div>}
 								</div>
 							</div>
+						</Link>
+					</div>
+					<button className={cx('usa-menu-btn')} onClick={() => setMobileMenuIsVisible(!mobileMenuIsVisible)}>
+						Menu
+						</button>
+				</div>
+				<nav
+					className={cx('usa-nav', { 'is-visible': mobileMenuIsVisible })}
+				>
+					<div className={cx('usa-nav__inner')}>
+						<button className={cx('usa-nav__close')} onClick={hideMenu}>
+							<img src={closeIcon} alt="close" />
+						</button>
+						<ul className={cx('usa-nav__primary usa-accordion')}>
+							{primaryNavItems.map((item, index) => (
+								<NavItem {...item} key={index} />
+							))}
+						</ul>
+						<div className={cx('usa-nav__secondary', 'usa-nav__secondary--extended')}>
+							<ul className={cx('usa-nav__secondary-links')}>
+								{secondaryNavItems.map((item, index) => (
+									<NavItem {...item} key={index} />
+								))}
+								{userFirstName && <NavItem type="secondary" title="Log out" path={logoutPath} />}
+							</ul>
+							<div className={cx('oec-logged-in-user')}>
+								{userFirstName ? (
+									<span>Hi, {userFirstName}.</span>
+								) : (
+										<Link to={loginPath}>Sign in</Link>
+									)}
+							</div>
 						</div>
-					</nav>
-				</header>
-			</div>
-		);
-	}
+					</div>
+				</nav>
+			</header >
+		</div >
+	);
 }
-
-export const Header = withRouter(HeaderWithoutRouter);
