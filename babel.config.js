5cd8ca93a50baa5555cbe780c6f7d94852d24522
jlhogan <julia@skylight.digital>
2020-09-02 18:12:30 -0400

    Fix borderless card styles (#53)

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,19 @@
 module.exports = function (api) {
-    api.cache(true);
+  api.cache(true);
 
-    const presets = [ "@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript" ];
-    const plugins = [ "macros", "@babel/proposal-class-properties", "@babel/proposal-object-rest-spread" ];
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ];
+  const plugins = [
+    'macros',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+  ];
 
-    return {
-      presets,
-      plugins
-    };
-}
+  return {
+    presets,
+    plugins,
+  };
+};
