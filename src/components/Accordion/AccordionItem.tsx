0805bf8fccc83812d7c217d4d33907c32b0a0fe2
jlhogan <julia@skylight.digital>
2020-09-15 13:34:59 -0400

    separate

---
+++ src/components/Accordion/AccordionItem.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export type AccordionItemProps = {
+	id: string;
+	title: JSX.Element | string;
+	expandText?: JSX.Element | string;
+	collapseText?: JSX.Element | string;
+	content: JSX.Element | string;
+	isExpanded?: boolean;	
+}
+
+type HeadingLevel = 'h2' | 'h3' | 'h4' | 'h5';
+type InternalAccordionItemProps = AccordionItemProps & {
+	headingLevel: HeadingLevel;
+}
+
+export const AccordionItem: React.FC<InternalAccordionItemProps> = ({
+	id,
+	title,
+	expandText,
+	collapseText,
+	content,
+	isExpanded: initialIsExpanded = false,
+	headingLevel,
+}) => {
+	const [isExpanded, setIsExpanded] = useState(initialIsExpanded);
+	const Heading = headingLevel;
+	return (
+		<>
+			<Heading className="oec-accordion__heading">
+				<button
+					className="oec-accordion__button"
+					onClick={() => setIsExpanded((e) => !e)}
+					aria-expanded={isExpanded}
+					aria-controls={id}
+				>
+					<div className="display-flex flex-justify flex-col flex-align-center padding-top-3 padding-bottom-3">
+						<div className="oec-accordion__heading-title">
+							{title}
+						</div>
+						<div className="oec-accordion__heading-expand">
+							{!isExpanded && (collapseText ? collapseText : expandText)}
+							{expandText}
+							{/* <AngleArrowDown className={cx("oec-accordion__button-icon", {'oec-accordion__button-icon--expanded': isExpanded})}/> */}
+						</div>
+					</div>
+				</button>
+			</Heading>
+			<div className="oec-accordion__content" id={id} aria-hidden={!isExpanded}>
+				{content}
+			</div>
+		</>
+	)
+}
