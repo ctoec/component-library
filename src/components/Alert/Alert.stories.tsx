960743f88c44d9da36f34cefea8fd5f4e8f20ff4
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2020-10-01 16:49:14 -0400

    Multi alert spacing (#75)

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -2,33 +2,54 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Alert } from '..';
+import { AlertProps } from '../..';
+
+const successAlertProps: AlertProps = {
+  type: 'success',
+  heading: 'Success alert!',
+  text: 'Good job, you.',
+};
+const warningAlertProps: AlertProps = {
+  type: 'warning',
+  heading: 'Watch out!',
+  text: 'Is that what you meant to do?',
+};
+const errorAlertProps: AlertProps = {
+  type: 'error',
+  heading: 'Hark, an error!',
+  text: "Something's not right.",
+};
+const infoAlertProps: AlertProps = {
+  type: 'info',
+  heading: 'Information',
+  text: 'Neutral information here.',
+};
+const allAlertProps: AlertProps[] = [
+  successAlertProps,
+  warningAlertProps,
+  errorAlertProps,
+  infoAlertProps,
+];
 
 storiesOf('Alert', module)
   .add('Success', () => {
-    return (
-      <Alert type={'success'} heading="Success alert!" text="Good job, you." />
-    );
+    return <Alert {...successAlertProps} />;
   })
   .add('Warning', () => {
-    return (
-      <Alert
-        type={'warning'}
-        heading="Watch out!"
-        text="Is that what you meant to do?"
-      />
-    );
+    return <Alert {...warningAlertProps} />;
   })
   .add('Error', () => {
-    return (
-      <Alert
-        type={'error'}
-        heading="Hark, an error!"
-        text="Something's not right."
-      />
-    );
+    return <Alert {...errorAlertProps} />;
   })
   .add('Info', () => {
+    return <Alert {...infoAlertProps} />;
+  })
+  .add('Lots of alerts', () => {
     return (
-      <Alert type={'info'} heading="Information" text="Neutral text here." />
+      <>
+        {allAlertProps.map((p, i) => (
+          <Alert {...p} key={i} />
+        ))}
+      </>
     );
   });
