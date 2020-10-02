7fdd5640953d22211ee2b1dc9e3324df72b31e2d
Melanie <melanie.mazanec@gmail.com>
2020-10-02 12:31:04 -0400

    Set margin top 3 on outside of fieldset

--- src/components/FieldSet/FieldSet.tsx
+++ src/components/FieldSet/FieldSet.tsx
@@ -41,39 +41,40 @@ export const FieldSet: React.FC<FieldSetProps> = ({
   }
 
   return (
-    <fieldset
-      className={cx(
-        'grid-gap',
-        'grid-row',
-        'usa-fieldset',
-        'margin-top-3',
-        {
-          [`usa-fieldset--${status && status.type}`]: status,
-        },
-        className
-      )}
-      id={id}
-      aria-describedby={ariaDescriber}
-      aria-invalid={status && status.type === 'error'}
-      disabled={disabled}
-    >
-      <legend id={`fieldset-legend-${id}`}>
-        {/* Needs to be wrapped in another el because spacing works differently for legends */}
-        <span
-          className={cx({
-            'usa-label': showLegend,
-            'usa-sr-only': !showLegend,
-            [`usa-label--${status && status.type}`]: showLegend && status,
-            'text-bold font-sans-lg': showLegend && legendStyle === 'title',
-          })}
-        >
-          {legend}
-          {optional && <span className="usa-hint">&nbsp;(optional)</span>}
-        </span>
-      </legend>
-      {hint && <span className="usa-hint text-italic">{hint}</span>}
-      {status && <FormStatus {...status} />}
-      <div className={childrenGroupClassName}>{children}</div>
-    </fieldset>
+    <div className="margin-top-3">
+      <fieldset
+        className={cx(
+          'grid-gap',
+          'grid-row',
+          'usa-fieldset',
+          {
+            [`usa-fieldset--${status && status.type}`]: status,
+          },
+          className
+        )}
+        id={id}
+        aria-describedby={ariaDescriber}
+        aria-invalid={status && status.type === 'error'}
+        disabled={disabled}
+      >
+        <legend id={`fieldset-legend-${id}`}>
+          {/* Needs to be wrapped in another el because spacing works differently for legends */}
+          <span
+            className={cx({
+              'usa-label': showLegend,
+              'usa-sr-only': !showLegend,
+              [`usa-label--${status && status.type}`]: showLegend && status,
+              'text-bold font-sans-lg': showLegend && legendStyle === 'title',
+            })}
+          >
+            {legend}
+            {optional && <span className="usa-hint">&nbsp;(optional)</span>}
+          </span>
+        </legend>
+        {hint && <span className="usa-hint text-italic">{hint}</span>}
+        {status && <FormStatus {...status} />}
+        <div className={childrenGroupClassName}>{children}</div>
+      </fieldset>
+    </div>
   );
 };
