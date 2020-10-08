e92e2577d668f9c45f9fd20eb7bf8d3fceb21569
Melanie <melanie.mazanec@gmail.com>
2020-10-08 11:36:04 -0400

    Remove default text truncation

--- src/components/TabNav/tabNavUtils.ts
+++ src/components/TabNav/tabNavUtils.ts
@@ -3,9 +3,6 @@ import { InternalTabItemType } from './Tabs';
 
 export function formatTabText(tabItem: TabItem) {
   let text: string | JSX.Element = tabItem.tabText;
-  if (text.length > 20) {
-    text = `${text.slice(0, 19)}...`;
-  }
   if (tabItem.tabTextFormatter) {
     text = tabItem.tabTextFormatter(text);
   }
