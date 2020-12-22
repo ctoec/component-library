382909a8ea4045a15944a6fcf0e25d6011885e97
Melanie <melanie.mazanec@gmail.com>
2020-12-22 16:37:53 -0500

    React dates initialize

--- src/components/DateInput/DateInput.stories.tsx
+++ src/components/DateInput/DateInput.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import 'react-dates/initialize';
 import { DateInput, FormStatusProps } from '..';
 
 const commonProps = {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Necessary to avoid typing errors
-import 'react-dates/initialize';
+import '../node_modules/react-dates/initialize.js';
 export * from './assets/images';
 export * from './components';
 export * from './hooks';
