525887f2755fcffce7996c6610e02fba6b822277
Melanie <melanie.mazanec@gmail.com>
2020-12-22 14:03:34 -0500

    Different rollup config to preserve structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,34 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import postcss from 'rollup-plugin-postcss';
 import svgr from '@svgr/rollup';
-import json from '@rollup/plugin-json';
+import sass from 'rollup-plugin-sass';
+import typescript from 'rollup-plugin-typescript2';
 import url from '@rollup/plugin-url';
 import copy from 'rollup-plugin-copy';
 
-const packageJson = require('./package.json');
-
+import pkg from './package.json';
+// continued
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: 'cjs',
+      exports: 'named',
       sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
+      strict: false,
     },
   ],
   plugins: [
-    // Don't include peer deps in bundle
-    peerDepsExternal(),
-    postcss({
-      minimize: true,
-      modules: true,
-      extract: true,
-      use: [['sass', { includePaths: ['src/components/_index.scss'] }]],
-    }),
     url(),
-    // Url + svgr allows us to import ReactComponent as SVGName and use it like a component
     svgr(),
-    json(),
+    sass({ insert: true }),
+    typescript({ objectHashIgnoreUnknownHack: true }),
     copy({
       targets: [
         { src: 'src/assets/images/*.svg', dest: 'dist/assets/images' },
         { src: 'src/assets/images/*.png', dest: 'dist/assets/images' },
       ],
     }),
-    // If true, the plugin will prefer built-in modules (e.g. fs, path)
-    resolve({ preferBuiltins: true }),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
   ],
+  external: ['react', 'react-dom'],
   inlineDynamicImports: true,
 };
