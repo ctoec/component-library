e6c1cab2ce94845c34cb8127d5543aa52734fe99
Melanie <melanie.mazanec@gmail.com>
2020-12-28 14:19:22 -0500

    Bump version number

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,99 +1,26 @@
-import React from 'react';
-import { SVGInline } from './SVGInline';
-
 // USWDS imports
-import arrowDown from 'uswds/dist/img/arrow-down.svg';
-import error from 'uswds/dist/img/alerts/error.svg';
-import info from 'uswds/dist/img/alerts/info.svg';
-import success from 'uswds/dist/img/alerts/success.svg';
+export { ReactComponent as ArrowDown } from './uswds/arrow-down.svg';
+export { ReactComponent as Error } from './uswds/alerts/error.svg';
+export { ReactComponent as Info } from './uswds/alerts/info.svg';
+export { ReactComponent as Success } from './uswds/alerts/success.svg';
 
-// Local imports
-import angleArrowDown from './angleArrowDown.svg';
-import arrowRight from './arrowRight.svg';
-import calendar from './calendar.svg';
-import doubleClipboard from './doubleClipboard.svg';
-import downArrowCircle from './downArrowCircle.svg';
-import fileDownload from './fileDownload.svg';
-import hero from './hero.svg';
-import history from './history.svg';
-import howToIcon from './howToIcon.svg';
-import logo from './logo.svg';
-import logoWithCheck from './logoWithCheck.svg';
-import pencil from './pencil.svg';
-import plusCircle from './plusCircle.svg';
-import questionIllustration from './questionIllustration.svg';
-import teacherWithChalkboard from './teacherWithChalkboard.svg';
-import downloadArrow from './download.svg';
-import supportIcon from './supportIcon.svg';
-import trashCan from './trashCan.svg';
-import x from './x.svg';
-
-export const ArrowDown = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={arrowDown} />
-);
-export const DoubleClipboard = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={doubleClipboard} />
-);
-export const DownloadArrow = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={downloadArrow} />
-);
-export const Error = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={error} />
-);
-export const Info = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={info} />
-);
-export const Success = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={success} />
-);
-export const AngleArrowDown = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={angleArrowDown} />
-);
-export const ArrowRight = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={arrowRight} />
-);
-export const Calendar = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={calendar} />
-);
-export const DownArrowCircle = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={downArrowCircle} />
-);
-export const FileDownload = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={fileDownload} />
-);
-export const Hero = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={hero} />
-);
-export const History = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={history} />
-);
-export const HowToIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={howToIcon} />
-);
-export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={logo} />
-);
-export const LogoWithCheck = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={logoWithCheck} />
-);
-export const Pencil = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={pencil} />
-);
-export const PlusCircle = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={plusCircle} />
-);
-export const QuestionIllustration = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={questionIllustration} />
-);
-export const SupportIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={supportIcon} />
-);
-export const TeacherWithChalkboard = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={teacherWithChalkboard} />
-);
-export const TrashCan = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={trashCan} />
-);
-export const X = (props: React.SVGProps<SVGSVGElement>) => (
-  <SVGInline svgProps={props} url={x} />
-);
+// Project specific imports
+export { ReactComponent as AngleArrowDown } from './angleArrowDown.svg';
+export { ReactComponent as ArrowRight } from './arrowRight.svg';
+export { ReactComponent as Calendar } from './calendar.svg';
+export { ReactComponent as DoubleClipboard } from './doubleClipboard.svg';
+export { ReactComponent as DownArrowCircle } from './downArrowCircle.svg';
+export { ReactComponent as DownloadArrow } from './download.svg';
+export { ReactComponent as FileDownload } from './fileDownload.svg';
+export { ReactComponent as Hero } from './hero.svg';
+export { ReactComponent as History } from './history.svg';
+export { ReactComponent as HowToIcon } from './howToIcon.svg';
+export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as LogoWithCheck } from './logoWithCheck.svg';
+export { ReactComponent as Pencil } from './pencil.svg';
+export { ReactComponent as PlusCircle } from './plusCircle.svg';
+export { ReactComponent as QuestionIllustration } from './questionIllustration.svg';
+export { ReactComponent as TeacherWithChalkboard } from './teacherWithChalkboard.svg';
+export { ReactComponent as SupportIcon } from './supportIcon.svg';
+export { ReactComponent as TrashCan } from './trashCan.svg';
+export { ReactComponent as X } from './x.svg';
