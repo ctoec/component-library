426b38fcafab8efa112b84e5e88c715549ac8c93
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2020-12-29 17:36:57 -0500

    457 retake (#107)
    Change exports for SVGs, add babel inline svg, pin USWDS version

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ];
+  const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'];
   const plugins = [
     'macros',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
+    'inline-react-svg',
   ];
 
   return {
--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,26 +1,54 @@
 // USWDS imports
-export { ReactComponent as ArrowDown } from './uswds/arrow-down.svg';
-export { ReactComponent as Error } from './uswds/alerts/error.svg';
-export { ReactComponent as Info } from './uswds/alerts/info.svg';
-export { ReactComponent as Success } from './uswds/alerts/success.svg';
+import { ReactComponent as ArrowDown } from './uswds/arrow-down.svg';
+import { ReactComponent as Error } from './uswds/alerts/error.svg';
+import { ReactComponent as Info } from './uswds/alerts/info.svg';
+import { ReactComponent as Success } from './uswds/alerts/success.svg';
 
 // Project specific imports
-export { ReactComponent as AngleArrowDown } from './angleArrowDown.svg';
-export { ReactComponent as ArrowRight } from './arrowRight.svg';
-export { ReactComponent as Calendar } from './calendar.svg';
-export { ReactComponent as DoubleClipboard } from './doubleClipboard.svg';
-export { ReactComponent as DownArrowCircle } from './downArrowCircle.svg';
-export { ReactComponent as DownloadArrow } from './download.svg';
-export { ReactComponent as FileDownload } from './fileDownload.svg';
-export { ReactComponent as Hero } from './hero.svg';
-export { ReactComponent as History } from './history.svg';
-export { ReactComponent as HowToIcon } from './howToIcon.svg';
-export { ReactComponent as Logo } from './logo.svg';
-export { ReactComponent as LogoWithCheck } from './logoWithCheck.svg';
-export { ReactComponent as Pencil } from './pencil.svg';
-export { ReactComponent as PlusCircle } from './plusCircle.svg';
-export { ReactComponent as QuestionIllustration } from './questionIllustration.svg';
-export { ReactComponent as TeacherWithChalkboard } from './teacherWithChalkboard.svg';
-export { ReactComponent as SupportIcon } from './supportIcon.svg';
-export { ReactComponent as TrashCan } from './trashCan.svg';
-export { ReactComponent as X } from './x.svg';
+import { ReactComponent as AngleArrowDown } from './angleArrowDown.svg';
+import { ReactComponent as ArrowRight } from './arrowRight.svg';
+import { ReactComponent as Calendar } from './calendar.svg';
+import { ReactComponent as DoubleClipboard } from './doubleClipboard.svg';
+import { ReactComponent as DownArrowCircle } from './downArrowCircle.svg';
+import { ReactComponent as DownloadArrow } from './download.svg';
+import { ReactComponent as FileDownload } from './fileDownload.svg';
+import { ReactComponent as Hero } from './hero.svg';
+import { ReactComponent as History } from './history.svg';
+import { ReactComponent as HowToIcon } from './howToIcon.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as LogoWithCheck } from './logoWithCheck.svg';
+import { ReactComponent as Pencil } from './pencil.svg';
+import { ReactComponent as PlusCircle } from './plusCircle.svg';
+import { ReactComponent as QuestionIllustration } from './questionIllustration.svg';
+import { ReactComponent as TeacherWithChalkboard } from './teacherWithChalkboard.svg';
+import { ReactComponent as SupportIcon } from './supportIcon.svg';
+import { ReactComponent as TrashCan } from './trashCan.svg';
+import { ReactComponent as X } from './x.svg';
+
+// Ostensibly this is unnecessary??
+// https://github.com/facebook/create-react-app/pull/5573
+export {
+  ArrowDown,
+  Error,
+  Info,
+  Success,
+  AngleArrowDown,
+  ArrowRight,
+  Calendar,
+  DoubleClipboard,
+  DownArrowCircle,
+  DownloadArrow,
+  FileDownload,
+  Hero,
+  History,
+  HowToIcon,
+  Logo,
+  LogoWithCheck,
+  Pencil,
+  PlusCircle,
+  QuestionIllustration,
+  TeacherWithChalkboard,
+  SupportIcon,
+  TrashCan,
+  X
+}
