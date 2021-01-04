538cdf00d37d42d1f9909f270fd59ca906868ee2
Melanie <melanie.mazanec@gmail.com>
2021-01-04 15:29:09 -0500

    Remove unused modal close side effect

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -20,43 +20,42 @@ export const Modal: React.FC<ModalProps> = ({
   showHeaderBorder = false,
   content,
 }) => (
-  <ReactModal
-    isOpen={isOpen}
-    // onRequestClose={onXClick}
-    shouldCloseOnEsc={true}
-    shouldCloseOnOverlayClick={true}
-    appElement={document.getElementById(appElementId) || undefined}
-    style={{
-      content: {
-        bottom: 'auto',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        maxHeight: '100%',
-        overflowY: 'scroll',
-      },
-    }}
-  >
-    <div
-      className={cx('grid-row display-flex flex-justify flex-align-center', {
-        'border-bottom border-base-lighter': showHeaderBorder,
-      })}
+    <ReactModal
+      isOpen={isOpen}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+      appElement={document.getElementById(appElementId) || undefined}
+      style={{
+        content: {
+          bottom: 'auto',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          maxHeight: '100%',
+          overflowY: 'scroll',
+        },
+      }}
     >
-      {header}
-      <div className="oec-modal-close">
-        <Button
-          appearance="unstyled"
-          text={
-            <div className="display-flex flex-column flex-align-center">
-              <X className="height-3 text-base-darker" />
-              <p className="margin-y-05">close</p>
-            </div>
-          }
-          onClick={onXClick}
-        />
+      <div
+        className={cx('grid-row display-flex flex-justify flex-align-center', {
+          'border-bottom border-base-lighter': showHeaderBorder,
+        })}
+      >
+        {header}
+        <div className="oec-modal-close">
+          <Button
+            appearance="unstyled"
+            text={
+              <div className="display-flex flex-column flex-align-center">
+                <X className="height-3 text-base-darker" />
+                <p className="margin-y-05">close</p>
+              </div>
+            }
+            onClick={onXClick}
+          />
+        </div>
       </div>
-    </div>
-    <div className={cx({ 'margin-top-2': showHeaderBorder })}>{content}</div>
-  </ReactModal>
-);
+      <div className={cx({ 'margin-top-2': showHeaderBorder })}>{content}</div>
+    </ReactModal>
+  );
