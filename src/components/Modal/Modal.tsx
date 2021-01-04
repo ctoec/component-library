e9ec6ec693d1bab556a46cd24d95a6578e523571
Melanie <melanie.mazanec@gmail.com>
2021-01-04 14:41:44 -0500

    Ditch potentially unnecessary modal param

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,7 +6,7 @@ import { X, Button } from '../..';
 export type ModalProps = {
   appElementId?: string;
   isOpen: boolean;
-  toggleOpen: () => void;
+  onXClick: () => void;
   header: JSX.Element;
   showHeaderBorder?: boolean;
   content: JSX.Element;
@@ -15,50 +15,48 @@ export type ModalProps = {
 export const Modal: React.FC<ModalProps> = ({
   appElementId = 'root',
   isOpen,
-  toggleOpen,
+  onXClick,
   header,
   showHeaderBorder = false,
   content,
-}) => {
-  return (
-    <ReactModal
-      isOpen={isOpen}
-      onRequestClose={toggleOpen}
-      shouldCloseOnEsc={true}
-      shouldCloseOnOverlayClick={true}
-      appElement={document.getElementById(appElementId) || undefined}
-      style={{
-        content: {
-          bottom: 'auto',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          maxHeight: '100%',
-          overflowY: 'scroll',
-        },
-      }}
+}) => (
+  <ReactModal
+    isOpen={isOpen}
+    // onRequestClose={onXClick}
+    shouldCloseOnEsc={true}
+    shouldCloseOnOverlayClick={true}
+    appElement={document.getElementById(appElementId) || undefined}
+    style={{
+      content: {
+        bottom: 'auto',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        maxHeight: '100%',
+        overflowY: 'scroll',
+      },
+    }}
+  >
+    <div
+      className={cx('grid-row display-flex flex-justify flex-align-center', {
+        'border-bottom border-base-lighter': showHeaderBorder,
+      })}
     >
-      <div
-        className={cx('grid-row display-flex flex-justify flex-align-center', {
-          'border-bottom border-base-lighter': showHeaderBorder,
-        })}
-      >
-        {header}
-        <div className="oec-modal-close">
-          <Button
-            appearance="unstyled"
-            text={
-              <div className="display-flex flex-column flex-align-center">
-                <X className="height-3 text-base-darker" />
-                <p className="margin-y-05">close</p>
-              </div>
-            }
-            onClick={toggleOpen}
-          />
-        </div>
+      {header}
+      <div className="oec-modal-close">
+        <Button
+          appearance="unstyled"
+          text={
+            <div className="display-flex flex-column flex-align-center">
+              <X className="height-3 text-base-darker" />
+              <p className="margin-y-05">close</p>
+            </div>
+          }
+          onClick={onXClick}
+        />
       </div>
-      <div className={cx({ 'margin-top-2': showHeaderBorder })}>{content}</div>
-    </ReactModal>
-  );
-};
+    </div>
+    <div className={cx({ 'margin-top-2': showHeaderBorder })}>{content}</div>
+  </ReactModal>
+);
