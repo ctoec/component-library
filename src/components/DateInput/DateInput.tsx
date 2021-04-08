19ba66723ba5a7bdfe8ffdfe62a50d066e8901e2
nickbristow <richard.nick.bristow@gmail.com>
2021-04-08 14:24:55 -0400

    test

--- src/components/DateInput/DateInput.tsx
+++ src/components/DateInput/DateInput.tsx
@@ -156,9 +156,11 @@ export const DateInput: React.FC<DateInputProps> = ({
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
             let val = event.target.value;
             val = val.replaceAll('/', '');
+            console.log('val', val);
+            console.log('vallength', val.length);
             if (val.length >= 5)
               val = `${val.substr(0, 1)}/${val.substr(2, 3)}/${val.substr(4)}`;
-            else if (val.length == 4)
+            else if (val.length === 4)
               val = `${val.substr(0, 1)}/${val.substr(2, 3)}/`;
             else if (val.length === 2) val = `${val.substr(0, 1)}/`;
 
